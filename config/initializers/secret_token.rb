712dac44c3711f2e715714407268e3c11897636c
FloatingLegs <FolaDrives@FloatingLegs.com>
2014-05-04 22:40:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '6028b63535e87846a6adacacd08869f718339ed334f2f3f1bc6d49ad15e9a51b9c7f7536f03b5aedb822407aace25f3c154738510f31f0c6f05261884a01ae02'
