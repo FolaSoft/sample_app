3b5b57314b4a5d135c150bf887f669385557c20c
FloatingLegs <FolaDrives@FloatingLegs.com>
2014-05-17 19:34:02 -0700

    Reach 9.2.3 Friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
   def show
   	@user = User.find(params[:id])
   end
@@ -20,7 +22,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #user = User.find(params[:id])
   end
 
   def update
@@ -45,4 +47,9 @@ class UsersController < ApplicationController
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end 
+
+     def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
